919501120e5e98ae2823bb08c04debc4d422f82f
RTG <igadget28@gmail.com>
2017-03-22 11:38:05 +0800

    Added 'resetall' command..

---
+++ src/_64FF00/PurePerms/cmd/ResetAll.php
@@ -0,0 +1,95 @@
+<?php
+
+/* 
+ * Copyright (C) 2017 RTG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace _64FF00\PurePerms\cmd;
+
+use _64FF00\PurePerms\PurePerms;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+
+use pocketmine\Player;
+
+use pocketmine\utils\TextFormat;
+
+class ResetAll extends Command implements PluginIdentifiableCommand {
+    
+        /*
+        PurePerms by 64FF00 (Twitter: @64FF00)
+
+          888  888    .d8888b.      d8888  8888888888 8888888888 .d8888b.   .d8888b.
+          888  888   d88P  Y88b    d8P888  888        888       d88P  Y88b d88P  Y88b
+        888888888888 888          d8P 888  888        888       888    888 888    888
+          888  888   888d888b.   d8P  888  8888888    8888888   888    888 888    888
+          888  888   888P "Y88b d88   888  888        888       888    888 888    888
+        888888888888 888    888 8888888888 888        888       888    888 888    888
+          888  888   Y88b  d88P       888  888        888       Y88b  d88P Y88b  d88P
+          888  888    "Y8888P"        888  888        888        "Y8888P"   "Y8888P"
+    */
+
+    private $plugin;
+
+    /**
+     * @param PurePerms $plugin
+     * @param $name
+     * @param $description
+     */
+    
+    public function __construct(PurePerms $plugin, $name, $description)
+    {
+        $this->plugin = $plugin;
+        
+        parent::__construct($name, $description);
+        
+        $this->setPermission("pperms.command.usrinfo");
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param $label
+     * @param array $args
+     * @return bool
+     */
+    
+    public function execute(CommandSender $sender, $label, array $args) {
+        
+        if(!$this->testPermission($sender)) 
+            return false;
+        
+        // Straight into the function...
+        
+        if($sender instanceof Player) {
+            
+            $sender->sendMessage("I'm sorry, you need to execute this command using Console!");
+            
+        }
+        else {
+            
+            $this->plugin->resetAll();
+            
+        }
+        
+    }
+    
+    public function getPlugin() {
+        return $this->plugin;
+    }
+    
+}
